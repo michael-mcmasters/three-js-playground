0f6ef2be74a9563caab88c597b5fef2eceff3917
Michael McMasters <jmcmasters411@gmail.com>
2021-06-20 19:03:45 -0400

    Add lights and orbit controls

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 import './style.css'
 import * as Three from 'three';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new Three.Scene();
 const camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
@@ -13,13 +14,21 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.render(scene, camera);
 
 const geometry = new Three.TorusGeometry(10, 3, 16, 100);
-const material = new Three.MeshBasicMaterial({ color: 0xFF6347, wireframe: true });
+const material = new Three.MeshStandardMaterial({ color: 0xFF6347, wireframe: false });
 const torus = new Three.Mesh(geometry, material);
 scene.add(torus);
 
-function testy() {
-  console.log("testy");
-}
+const pointLight = new Three.PointLight(0xffffff);
+pointLight.position.set(5, 5, 5);
+
+const ambientLight = new Three.AmbientLight(0xffffff);
+scene.add(pointLight, ambientLight);
+
+const lightHelper = new Three.PointLightHelper(pointLight);
+const gridHelper = new Three.GridHelper(200, 50);
+scene.add(lightHelper, gridHelper);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 
 // Runs every frame.
@@ -31,6 +40,7 @@ function animate() {
 
   torus.rotation.x += 0.01;
 
+  controls.update();
   renderer.render(scene, camera);
 }
 animate();  // Call the recursive function once to begin loop.
